30d7a99d5a281fd14066b676721ce518f19bf916
Terry Blue <terryblue@comcast.net>
2020-01-16 23:34:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-/* Your code goes here */
->>>>>>> deaf6edef854ae7c66e9f64e6f2a97d6b8749c83
 var customerName = 'bob'
 const leastFavoriteCustomer = 'bob'
 
@@ -20,7 +16,3 @@ function overWriteBestCustomer() {
 function changeLeastFavoriteCustomer() {
   leastFavoriteCustomer = 'not bob'
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> deaf6edef854ae7c66e9f64e6f2a97d6b8749c83
